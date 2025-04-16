41403eb8f505672058f9956754379880b167ee75
Arzianghanchi <arzian.ghanchi@clouddrove.com>
2025-04-16 18:39:21 +0000

    add/missing-var.enable-variable

--- main.tf
+++ main.tf
@@ -1196,7 +1196,7 @@ resource "aws_wafv2_web_acl" "main" {
 # WAFv2 web acl association with ALB
 #####
 resource "aws_wafv2_web_acl_association" "main" {
-  count = var.waf_enabled && var.web_acl_association && length(var.resource_arn_list) == 0 ? 1 : 0
+  count = var.enable && var.waf_enabled && var.web_acl_association && length(var.resource_arn_list) == 0 ? 1 : 0
 
   resource_arn = var.resource_arn
   web_acl_arn  = join("", aws_wafv2_web_acl.main[*].arn)
